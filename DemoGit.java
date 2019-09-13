b8f14b5676d890ebb89d458df43e9a108c007ee4
Rijul5 <rijulsaini.mait@gmail.com>
2019-09-13 13:10:56 -0400

    first commit

---
+++ DemoGit.java
@@ -0,0 +1,13 @@
+public class DemoGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Class. This is a demo program");
+		int abc = divison_method(32,4);
+		System.out.println(abc);
+
+	}
+
+	private static int divison_method(int i, int j) {
+		return i/j;
+	}
+}
